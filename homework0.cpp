ae1787a1274e2efe4935f8b561c1036501631882
bplastow <blaine.plastow@iconfitness.com>
2016-09-06 11:26:28 -0600

    updated .cpp file

--- homework0.cpp
+++ homework0.cpp
@@ -10,6 +10,7 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 #include "avl.h"
 
@@ -17,16 +18,61 @@ using namespace std;
 
 int main()
 {
-	int treekeys[9] = {36, 17, 5, 30, 28, 81, 37, 76, 83};
+	int option;
+	//int treekeys[9] = {36, 17, 5, 30, 28, 81, 37, 76, 83};
 	avl mytree;
 
-	cout << "printing the tree in order\nbefore adding numbers\n";
+	cout << "Choose the option you want to perform" << endl;
 
-	mytree.printinorder();
+	cout << "1. Create_Tree" << endl;
+	cout << "2. Insertion" << endl;
+	cout << "3. Deletion" << endl;
+	cout << "4. Search" << endl;
+	cout << "5. Traversal" << endl;
+	cout << "6. Delete_Tree" << endl;
+	cout << "7. Check_Balance" << endl;
 
-	for(int i = 0; i < 9; i++)
+	cin >> option;
+
+	int output;
+	ifstream file;
+
+	switch(option)
 	{
-		 mytree.insert(treekeys[i]);
+		case 1:
+			file.open("input.txt");
+			if(file.is_open())
+			{
+				while(!file.eof())
+				{
+					file >> output;
+					//cout << output << endl;
+					mytree.insert(output);
+				}
+			}
+			file.close();
+			break;
+		case 2:
+			cout << "case 2";
+			break;
+		case 3:
+			cout << "case 2";
+			break;
+		case 4:
+			cout << "case 2";
+			break;
+		case 5:
+			cout << "case 2";
+			break;
+		case 6:
+			cout << "case 2";
+			break;
+		case 7:
+			cout << "case 2";
+			break;
+		default:
+			cout << "Not a valid option try again";
+			break;
 	}
 
 	cout << "printing the tree in order\nafter adding numbers\n";
